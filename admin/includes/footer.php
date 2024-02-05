c8ff9c962d42c924e28bfc91db087decec0575ed
Teo <teosany@icloud.com>
2024-02-05 16:06:18 +0100

    add zeabur

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,4 +1,7 @@
 <footer class="bg-light text-center text-lg-start mt-auto">
+    <div class="text-center p-3">
+        <a class="mx-auto p-3" href="https://zeabur.com?referralCode=Teosany&utm_source=Teosany"><img src=https://zeabur.com/deployed-on-zeabur-dark.svg alt="Deployed on Zeabur"/></a>
+    </div>
     <div class="text-end p-3">
         &copy; 2024 Gestion de librairie en ligne
     </div>

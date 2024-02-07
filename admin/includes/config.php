355fec05792d652d016f0aae178c5295984c7c70
Teo <teosany@icloud.com>
2024-02-07 15:19:48 +0100

    fix38

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -4,20 +4,18 @@ const DB_USER = 'root';
 const DB_PASS = 'root';
 const DB_NAME = 'library';
 
-try
-{
-    try
-    {
-        $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
-    } catch (PDOException $e) {
-        exit("Error: " . $e->getMessage());
+try {
+    if (empty($_ENV["DB_HOST"])) {
+        try {
+            $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+        } catch (PDOException $e) {
+            exit("Error: " . $e->getMessage());
+        }
     }
     if (!empty($_ENV["DB_HOST"])) {
-        $dbh = new PDO("mysql:host=".$_ENV["DB_HOST"].";dbname=".$_ENV["DB_NAME"],$_ENV["DB_USER"], $_ENV["DB_PASS"]);
+        $dbh = new PDO("mysql:host=" . $_ENV["DB_HOST"] . ";dbname=" . $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
     }
-}
-catch (PDOException $e)
-{
+} catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
 

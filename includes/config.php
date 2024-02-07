d9e225520f8483dada5ffc2c9ac0d29a5598f0b6
Teo <teosany@icloud.com>
2024-02-07 15:17:26 +0100

    fix36

--- includes/config.php
+++ includes/config.php
@@ -1,22 +1,20 @@
 <?php
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','root');
-define('DB_NAME','library');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'library');
 
-try
-{
-    try
-    {
-        $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
-    } catch (PDOException $e) {
-        exit("Error: " . $e->getMessage());
+try {
+    if (empty($_ENV["DB_HOST"])) {
+        try {
+            $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+        } catch (PDOException $e) {
+            exit("Error: " . $e->getMessage());
+        }
     }
     if (!empty($_ENV["DB_HOST"])) {
-        $dbh = new PDO("mysql:host=".$_ENV["DB_HOST"].";dbname=".$_ENV["DB_NAME"],$_ENV["DB_USER"], $_ENV["DB_PASS"]);
+        $dbh = new PDO("mysql:host=" . $_ENV["DB_HOST"] . ";dbname=" . $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
     }
-}
-catch (PDOException $e)
-{
+} catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }

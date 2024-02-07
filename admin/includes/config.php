4bdc6ab4b3d1769f5b1817327c9eea3f395c74b9
Teo <teosany@icloud.com>
2024-02-07 15:13:52 +0100

    fix35

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,17 +1,23 @@
 <?php
-define('DB_HOST','fra1.clusters.zeabur.com:31929');
+define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','L7xbU0JPGQtW3VwCv5F9D842Yrnpe61q');
+define('DB_PASS','root');
 define('DB_NAME','library');
 
 try
 {
-    // Connexion � la base
-    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
+    try
+    {
+        $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
+    } catch (PDOException $e) {
+        exit("Error: " . $e->getMessage());
+    }
+    if (!empty($_ENV["DB_HOST"])) {
+        $dbh = new PDO("mysql:host=".$_ENV["DB_HOST"].";dbname=".$_ENV["DB_NAME"],$_ENV["DB_USER"], $_ENV["DB_PASS"]);
+    }
 }
 catch (PDOException $e)
 {
-    // Echec de la connexion
     exit("Error: " . $e->getMessage());
 }
 

24b9d33c9aac9e19b1d140f485903c5dca96764b
Teo <teosany@icloud.com>
2024-02-07 09:57:52 +0100

    fix10

--- captcha.php
+++ captcha.php
@@ -6,11 +6,11 @@ $text = rand(10000,99999);
 $_SESSION["vercode"] = $text;
 
 $height = 25;
-$width = 65;   
+$width = 65;
 $image = imagecreate($width, $height);
 
 $black = imagecolorallocate($image, 0, 0, 0);
-$white = imagecolorallocate($image, 255, 255, 255); 
+$white = imagecolorallocate($image, 255, 255, 255);
 $font_size = 14;
 
 imagestring($image, $font_size, 5, 5, $text, $white);

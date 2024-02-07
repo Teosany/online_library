2b47077ce4161e89804c1b3383888f3fad2ffc69
Teo <teosany@icloud.com>
2024-02-07 11:57:00 +0100

    fix23

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,16 @@
 <?php
 // Configuration de la connexion
-//define('DB_HOST','localhost');
-//define('DB_USER','root');
-//define('DB_PASS','root');
-//define('DB_NAME','library');
+define('DB_HOST','fra1.clusters.zeabur.com:31929');
+define('DB_USER','root');
+define('DB_PASS','L7xbU0JPGQtW3VwCv5F9D842Yrnpe61q');
+define('DB_NAME','zeabur');
 
 try
 {
     // Connexion � la base
 //    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
-    $dbh = new PDO("mysql:host=fra1.clusters.zeabur.com;dbname=zeabur",'root', 'L7xbU0JPGQtW3VwCv5F9D842Yrnpe61q');
-//    $dbh = new PDO("mysql://root:L7xbU0JPGQtW3VwCv5F9D842Yrnpe61q@fra1.clusters.zeabur.com:31929/zeabur");
+    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
+    $dbh = new PDO("mysql://root:L7xbU0JPGQtW3VwCv5F9D842Yrnpe61q@fra1.clusters.zeabur.com:31929/zeabur");
 //    $dbh = $DATABASE_URL;
 }
 catch (PDOException $e)
